7c55d75a2f0cb458952bb6ca557b06618ff244bd
atec <atecce@urbn.com>
2018-11-25 00:29:08 -0500

    log with timestamps

--- main.go
+++ main.go
@@ -1,34 +1,38 @@
 package main
 
 import (
-	"strings"
 	"compress/gzip"
-	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/yhat/scrape"
 
 	"github.com/gocolly/colly"
 )
 
+const domain = "https://www.gutenberg.org/"
+
+var dir = filepath.Join("/", "keybase", "public", "atec", "data", "gutenberg")
+
 func main() {
 
-	const domain = "https://www.gutenberg.org/"
+	// TODO pick up where you left off
 
 	authorCollector := colly.NewCollector()
 
 	authorCollector.OnRequest(func(r *colly.Request) {
-		fmt.Println("[INFO] get", r.URL)
+		log.Println("[INFO] get", r.URL)
 	})
 
 	authorCollector.OnHTML("h2", func(e *colly.HTMLElement) {
 
 		author := e.ChildText("a")
 
-		path := filepath.Join("/", "keybase", "public", "atec", "data", "gutenberg", author)
+		path := filepath.Join(dir, author)
 		if _, err := os.Stat(path); os.IsNotExist(err) {
 			_ = os.Mkdir(path, 0700)
 		}
@@ -41,23 +45,23 @@ func main() {
 
 				if strings.Contains(href, "wikipedia") {
 					continue
-				} 
+				}
 
 				url := domain + href + ".txt.utf-8"
 				name := filepath.Join(path, title+".txt.gz")
 
-				fmt.Println("[INFO] getting", url)
+				log.Println("[INFO] getting", url)
 				res, err := http.Get(url)
 				if err != nil {
-					fmt.Println("[ERR]", err)
+					log.Println("[ERR]", err)
 					continue
 				}
 				defer res.Body.Close()
 
-				fmt.Println("[INFO] creating", name)
+				log.Println("[INFO] creating", name)
 				f, err := os.Create(name)
 				if err != nil {
-					fmt.Println("[ERR]", err)
+					log.Println("[ERR]", err)
 					continue
 				}
 				defer f.Close()
@@ -65,10 +69,10 @@ func main() {
 				w := gzip.NewWriter(f)
 				defer w.Close()
 
-				fmt.Println("[INFO] copying")
+				log.Println("[INFO] copying")
 				_, err = io.Copy(w, res.Body)
 				if err != nil {
-					fmt.Println("[ERR]", err)
+					log.Println("[ERR]", err)
 					continue
 				}
 			}
