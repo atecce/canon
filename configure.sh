08043c25709fdfdfa211e82b5d8a8852d69d2bb2
atec <root@atec.pub>
2018-12-03 18:02:56 -0500

    build proxy. enable services on config. start services on deploy

--- configure.sh
+++ configure.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-gcloud compute ssh canon --command="sudo yum install -y wget"
-gcloud compute ssh canon --command="wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.5.1.rpm"
-gcloud compute ssh canon --command="wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.5.1.rpm.sha512"
-# TODO gcloud compute ssh canon --command="shasum -a 512 -c elasticsearch-6.5.1.rpm.sha512"
-gcloud compute ssh canon --command="sudo rpm --install elasticsearch-6.5.1.rpm"
-gcloud compute ssh canon --command="sudo yum install -y java-sdk"
-gcloud compute ssh canon --command="sudo yum install -y elasticsearch"
+GOOS=linux GOARCH=386 go build github.com/atecce/proxy
+gcloud compute scp proxy ~/go/src/github.com/atecce/proxy/proxy.service atec@canon:~
+gcloud compute ssh atec@canon --command="sudo mv /home/atec/proxy.service /etc/systemd/system/"
+gcloud compute ssh atec@canon --command="sudo mv /home/atec/proxy /usr/sbin/"
+
+gcloud compute ssh atec@canon --command="sudo yum install -y wget"
+# TODO gcloud compute ssh atec@canon --command="sudo yum install -y shasum"
+gcloud compute ssh atec@canon --command="wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.5.1.rpm"
+gcloud compute ssh atec@canon --command="wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.5.1.rpm.sha512"
+# TODO gcloud compute ssh atec@canon --command="shasum -a 512 -c elasticsearch-6.5.1.rpm.sha512"
+gcloud compute ssh atec@canon --command="sudo rpm --install elasticsearch-6.5.1.rpm"
+gcloud compute ssh atec@canon --command="sudo yum install -y java-sdk"
+gcloud compute ssh atec@canon --command="sudo yum install -y elasticsearch"
+
+gcloud compute ssh canon --command="sudo systemctl daemon-reload"
+gcloud compute ssh canon --command="sudo systemctl enable proxy.service"
+gcloud compute ssh canon --command="sudo systemctl enable elasticsearch.service"
--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-gcloud compute ssh canon --command="sudo systemctl daemon-reload"
-gcloud compute ssh canon --command="sudo systemctl enable elasticsearch.service"
+gcloud compute ssh canon --command="sudo systemctl start proxy.service"
+gcloud compute ssh canon --command="sudo systemctl start elasticsearch.service"
