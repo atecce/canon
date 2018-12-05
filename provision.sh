c785b5389179bcc3db5d1607a78b957b70a803e1
atec <atecce@urbn.com>
2018-12-05 10:29:42 -0500

    provision canon with 100GB

--- provision.sh
+++ provision.sh
@@ -2,5 +2,8 @@
 
 gcloud compute instances create canon \
     --image-family centos-7 --image-project centos-cloud \
-    --address canon --tags http-server,https-server #\
-# TODO    --create-disk=size=100GB
+    --address canon --tags http-server,https-server
+
+gcloud compute disks resize canon --size 100GB
+
+gcloud compute instances reset canon
