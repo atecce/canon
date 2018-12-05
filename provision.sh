0adf20ef9782e6cb8f84563a3a9cf1db32fd6206
atec <root@atec.pub>
2018-12-04 20:25:39 -0500

    use address and 100GB disk

--- provision.sh
+++ provision.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 # TODO need proxy to port 9000
-gcloud compute instances create canon --image-family centos-7 --image-project centos-cloud --tags http-server,https-serve
+gcloud compute instances create canon \
+    --image-family centos-7 --image-project centos-cloud \
+    --address canon --tags http-server,https-server \
+    --create-disk=size=100GB
