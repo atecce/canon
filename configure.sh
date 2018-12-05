7ad3748091669ae35e8f3fd19d4dd4e1288928e7
atec <atecce@urbn.com>
2018-12-05 10:47:10 -0500

    group non-elastic installations

--- configure.sh
+++ configure.sh
@@ -9,10 +9,11 @@ gcloud compute ssh atec@canon --command="sudo mv /home/atec/proxy /usr/sbin/"
 
 gcloud compute ssh atec@canon --command="sudo yum install -y wget"
 gcloud compute ssh atec@canon --command="sudo yum install -y perl-Digest-SHA"
+gcloud compute ssh atec@canon --command="sudo yum install -y java-sdk"
+
 gcloud compute ssh atec@canon --command="wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.5.1.rpm"
 gcloud compute ssh atec@canon --command="wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.5.1.rpm.sha512"
 gcloud compute ssh atec@canon --command="shasum -a 512 -c elasticsearch-6.5.1.rpm.sha512"
-gcloud compute ssh atec@canon --command="sudo yum install -y java-sdk"
 gcloud compute ssh atec@canon --command="sudo rpm --install elasticsearch-6.5.1.rpm"
 gcloud compute ssh atec@canon --command="sudo yum install -y elasticsearch"
 
