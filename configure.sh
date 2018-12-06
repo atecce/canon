18dee8f728662d52b6054241ccf0ab133f7ad402
atec <root@atec.pub>
2018-12-05 19:20:53 -0500

    distribute proxy config and bin through keybase

--- configure.sh
+++ configure.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # TODO distribute release?
-GOOS=linux GOARCH=386 go build github.com/atecce/proxy
-
-gcloud compute scp proxy ~/go/src/github.com/atecce/proxy/proxy.service atec@canon:~
-gcloud compute ssh atec@canon --command="sudo mv /home/atec/proxy.service /etc/systemd/system/"
-gcloud compute ssh atec@canon --command="sudo mv /home/atec/proxy /usr/sbin/"
 
 gcloud compute ssh atec@canon --command="sudo yum install -y wget"
 gcloud compute ssh atec@canon --command="sudo yum install -y perl-Digest-SHA"
 gcloud compute ssh atec@canon --command="sudo yum install -y java-sdk"
+
+gcloud compute ssh atec@canon --command="wget https://atec.keybase.pub/bin/proxy"
+gcloud compute ssh atec@canon --command="wget https://atec.keybase.pub/etc/proxy.service"
+gcloud compute ssh atec@canon --command="sudo mv /home/atec/proxy.service /etc/systemd/system/"
+gcloud compute ssh atec@canon --command="sudo mv /home/atec/proxy /usr/sbin/"
 
 gcloud compute ssh atec@canon --command="wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.5.1.rpm"
 gcloud compute ssh atec@canon --command="wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.5.1.rpm.sha512"
