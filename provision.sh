d40208fe99481a42ab44cf40d28723f4b6334235
atec <root@atec.pub>
2018-12-07 18:48:38 -0500

    scale up

--- provision.sh
+++ provision.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 gcloud compute instances create canon \
+    --machine-type n1-standard-8 \
     --image-family centos-7 --image-project centos-cloud \
     --address canon --tags http-server,https-server
 
