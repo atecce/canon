b50fdd58b9b22d176c1fa40d80a18eb8e224bfce
atec <root@atec.pub>
2018-12-25 15:50:07 -0500

    tar legacy data

--- clean/main.go
+++ clean/main.go
@@ -1,29 +1,61 @@
 package main
 
 import (
-	"io/ioutil"
+	"archive/tar"
+	"compress/gzip"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
 	"strings"
 
-	"github.com/kr/pretty"
-
 	"github.com/atecce/canon/lib"
 )
 
 func main() {
-	infos, _ := ioutil.ReadDir(lib.Dir)
-	for _, info := range infos {
-		path := filepath.Join(lib.Dir, info.Name())
-		if strings.Contains(path, "¶") {
-			newPath := strings.Replace(path, "¶", "", -1)
-			log.Println("[INFO] renaming", path, "to", newPath)
-			err := os.Rename(path, newPath)
+
+	tarball, err := os.Create(filepath.Join(lib.Dir, "text.tar.gz"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	gzw := gzip.NewWriter(tarball)
+	defer gzw.Close()
+
+	tw := tar.NewWriter(gzw)
+	defer tw.Close()
+
+	if err := filepath.Walk(lib.Dir, func(path string, info os.FileInfo, err error) error {
+
+		if strings.Contains(path, ".txt.") && info.Mode().IsRegular() {
+
+			log.Println("tarring", path)
+
+			header, err := tar.FileInfoHeader(info, info.Name())
 			if err != nil {
-				linkErr := err.(*os.LinkError)
-				pretty.Logln("[ERR]", linkErr)
+				return err
+			}
+
+			header.Name = strings.TrimPrefix(strings.Replace(path, lib.Dir, "", -1), string(filepath.Separator))
+
+			if err := tw.WriteHeader(header); err != nil {
+				return err
+			}
+
+			f, err := os.Open(path)
+			if err != nil {
+				return err
+			}
+			defer f.Close()
+
+			if _, err := io.Copy(tw, f); err != nil {
+				return err
 			}
 		}
+
+		return nil
+
+	}); err != nil {
+		log.Fatal(err)
 	}
 }
