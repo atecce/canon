323c0cfeea404787f493dc5b416faee4fd713fad
atec <root@atec.pub>
2019-01-02 14:32:28 -0600

    pass disk size as command flag

--- provision.sh
+++ provision.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 gcloud compute instances create canon \
-    --machine-type n1-standard-8 \
+    --machine-type n1-standard-8 --boot-disk-size 100GB \
     --image-family centos-7 --image-project centos-cloud \
     --address canon --tags http-server,https-server
-
-gcloud compute disks resize canon --size 100GB
-
-gcloud compute instances reset canon
