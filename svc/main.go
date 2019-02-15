759f47fc19393e0fb485ba7fa8b6ddef3ed00bba
atec <root@atec.pub>
2019-02-15 10:59:27 -0500

    mv data out of repo

--- svc/main.go
+++ svc/main.go
@@ -6,6 +6,6 @@ import (
 )
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("gutenberg")))
+	http.Handle("/", http.FileServer(http.Dir("/usr/local/var/canon")))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
