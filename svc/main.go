bbd24634249e3e9f0e8ac5ad79ce04e8cccaddef
atec <root@atec.pub>
2019-02-15 11:06:55 -0500

    point directly at gutenberg for now

--- svc/main.go
+++ svc/main.go
@@ -6,6 +6,6 @@ import (
 )
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("/usr/local/var/canon")))
+	http.Handle("/", http.FileServer(http.Dir("/usr/local/var/canon/gutenberg")))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
