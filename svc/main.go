8e1105a9b180d2b87fed61f02523a4c22b1b9b35
atec <root@atec.pub>
2019-02-24 21:44:57 -0500

    poc for fuzzy search

--- svc/main.go
+++ svc/main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"io/ioutil"
+	"log"
 	"net/http"
 	"path/filepath"
 	"strings"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/lithammer/fuzzysearch/fuzzy"
 )
+
+var authors []string
 
 func main() {
 	e := echo.New()
@@ -29,6 +33,26 @@ func main() {
 
 		for _, fi := range fis {
 			if _, err := res.Write([]byte(fi.Name() + "\n")); err != nil {
+				return err
+			}
+			// time.Sleep(time.Minute)
+		}
+
+		return nil
+	})
+
+	e.GET("/search/:pattern", func(c echo.Context) error {
+
+		pattern := c.Param("pattern")
+
+		res := c.Response()
+
+		res.WriteHeader(http.StatusOK)
+
+		found := fuzzy.Find(pattern, authors)
+
+		for _, author := range found {
+			if _, err := res.Write([]byte(author + "\n")); err != nil {
 				return err
 			}
 			// time.Sleep(time.Minute)
@@ -67,6 +91,16 @@ func main() {
 
 		return c.String(http.StatusOK, string(b))
 	})
+
+	fis, err := ioutil.ReadDir("/var/canon/gutenberg/")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, fi := range fis {
+		author := fi.Name()
+		authors = append(authors, strings.TrimSuffix(author, filepath.Ext(author)))
+	}
 
 	e.StartTLS(":443", "/etc/canon/server.crt", "/etc/canon/server.key")
 }
