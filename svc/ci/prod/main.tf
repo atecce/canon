eb9192f35f7864af371706f1fa5b01ae3c4400bf
atec <root@atec.pub>
2019-02-25 16:04:35 -0500

    get prod to parity with dev

--- svc/ci/prod/main.tf
+++ svc/ci/prod/main.tf
@@ -19,12 +19,13 @@ provider "google" {
 }
 
 resource "google_compute_firewall" "canon-ssh" {
-    
+
     name = "canon-ssh"
     network = "default"
     target_tags = ["canon"]
 
-    source_ranges = ["141.158.1.238"]
+    source_ranges = ["141.158.1.238","100.19.46.101"]
+
     allow = {
         protocol = "tcp"
         ports = ["22"]
@@ -32,12 +33,28 @@ resource "google_compute_firewall" "canon-ssh" {
 }
 
 resource "google_compute_firewall" "canon" {
-
+    
     name = "canon"
     network = "default"
     target_tags = ["canon"]
 
-    source_ranges = ["0.0.0.0/0"]
+    # https://www.cloudflare.com/ips-v4
+    source_ranges = [
+        "173.245.48.0/20",
+        "103.21.244.0/22",
+        "103.22.200.0/22",
+        "103.31.4.0/22",
+        "141.101.64.0/18",
+        "108.162.192.0/18",
+        "190.93.240.0/20",
+        "188.114.96.0/20",
+        "197.234.240.0/22",
+        "198.41.128.0/17",
+        "162.158.0.0/15",
+        "104.16.0.0/12",
+        "172.64.0.0/13",
+        "131.0.72.0/22"
+    ]
     allow = {
         protocol = "tcp"
         ports = ["443"]
@@ -76,13 +93,18 @@ resource "google_compute_instance" "default" {
             timeout = "120s"
         }
         inline = [
-            "sudo mkdir -p /etc/canon",
-
             "sudo yum install -y wget",
+            
+            "sudo mkdir -p /etc/canon",
 
             "wget https://atec.keybase.pub/etc/sshd_config",
             "sudo mv sshd_config /etc/ssh/sshd_config",
             "sudo systemctl restart sshd.service",
+
+            "sudo mkdir -p /var/canon",
+
+            "wget https://atec.keybase.pub/data/gutenberg/entities.tar",
+            "sudo tar -xvf entities.tar -C /var/canon/"
         ]
     }
 
@@ -115,32 +137,8 @@ resource "google_compute_instance" "default" {
             private_key = "${file("~/.ssh/google_compute_engine")}"
             timeout = "120s"
         }
-        source = "/keybase/public/atec/bin/canon"
-        destination = "/usr/sbin/canon"
-    }
-
-    provisioner "file" {
-        connection = {
-            type = "ssh"
-            user = "root"
-            private_key = "${file("~/.ssh/google_compute_engine")}"
-            timeout = "120s"
-        }
-        source = "canon.service"
+        source = "../../canon.service"
         destination = "/etc/systemd/system/canon.service"
-    }
-
-    provisioner "remote-exec" {
-        connection = {
-            type = "ssh"
-            user = "root"
-            private_key = "${file("~/.ssh/google_compute_engine")}"
-            timeout = "120s"
-        }
-        inline = [
-            "chmod 755 /usr/sbin/canon",
-            "systemctl start canon.service",
-        ]
     }
 
     depends_on = ["google_compute_firewall.canon"]
