87737806baa43bff1763b62e37bd99d881db79ba
atec <root@atec.pub>
2019-03-25 11:40:47 -0400

    rip out comments

--- main.go
+++ main.go
@@ -1,19 +1,7 @@
-// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
+// The strength of the canonical is manifested in the quiet persistence of the strongest writers
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-// The strength of the canonical is manifested in the quiet persistence of the strongest writers.
-// Their fecundity is endless because they represent the heart and the head rather than the loins or the privileges of caste or sect or race
+// Their fecundity is endless because they represent the heart and the head
+// rather than the loins or the privileges of caste or sect or race
 
 package main
 
