3940bdb49f8250fc70d5cba265ed6f8351d3ebd6
atec <root@atec.pub>
2019-03-26 15:08:16 -0400

    wip: clean not founds

--- clean.sh
+++ clean.sh
@@ -1,2 +1,4 @@
+# TODO format properly and prevent two passes over the data
+fd -t f | while read f; do if grep "404 Not Found" $f; then echo $f; rm $f; fi; done
 fd -t f | while read f; do cat "$f" | if grep -q -E "003c.*003e.*File.*Found.*003c.*003e.*003c.*"; then echo "$f"; cat "$f"; rm "$f"; fi; done
 fd -t d -x rmdir
